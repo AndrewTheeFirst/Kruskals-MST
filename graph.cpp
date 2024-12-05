af06c16d0281979f1d73adf3379d6c328f3a7165
ANDREWIDEAPAD\03dre <pattona@southern.edu>
2024-12-05 16:15:42 -0500

    Implemented kruskal's algorithm, fixed bug in constructor (not clearing distances vector), weight method no longer counts dupes, and other misc tweaks.

--- graph.cpp
+++ graph.cpp
@@ -7,6 +7,21 @@
 #include <unordered_map>
 #include <queue>
 
+#include <iostream>
+
+struct edge{
+    size_t vertex_1, vertex_2, weight;
+
+    edge(size_t v1, size_t v2, size_t w): vertex_1(v1), vertex_2(v2), weight(w){}
+};
+
+struct compare_function{
+    bool operator()(edge* edge_1, edge* edge_2){
+        // True means e1 is heavier than e2, False means e1 is lighter than e2. 
+        // Therefore we want e1 to be heavier when e1 > e2
+        return edge_1->weight > edge_2->weight; 
+    }
+};
 
 Graph::Graph(const std::string& filename){
     std::string line, city, distance;
@@ -17,11 +32,11 @@ Graph::Graph(const std::string& filename){
     std::getline(file, line); // discards the first line in the txt file
 
     for(size_t line_index = 0; std::getline(file, line); line_index++){
-        std::istringstream line_stream(line);
-        line_stream >> city;
-        label_to_vertex_map.insert({city, line_index});
-        vertex_to_label_map.push_back(city);
-        
+        line_stream = std::istringstream(line);
+        line_stream >> city; // first word of each line is a city
+        this->label_to_vertex_map.insert({city, line_index});
+        this->vertex_to_label_map.push_back(city);
+        distances = std::vector<int>(); // ensures vector is clear before every loop
         while (line_stream >> distance){
             if (distance == "-"){
                 distances.push_back(NO_CONNECTION);
@@ -30,9 +45,8 @@ Graph::Graph(const std::string& filename){
                 distances.push_back(std::stoi(distance));
             }
         }
-        adjacency_matrix.push_back(distances);
+        this->adjacency_matrix.push_back(distances);
     }
-
 }
 
 size_t Graph::size() const{
@@ -40,47 +54,65 @@ size_t Graph::size() const{
 }
 
 int Graph::weight() const {
-    size_t num_cities = adjacency_matrix.size();
+    size_t num_cities = this->adjacency_matrix.size();
+    int weight = 0;
     int total_weight = 0;
     for (size_t city_index = 0; city_index < num_cities; city_index++){
-        for (size_t weight_index = 0;  adjacency_matrix[city_index].size(); weight_index++){
-            total_weight += adjacency_matrix[city_index][weight_index];
+        for (size_t weight_index = city_index + 1; weight_index < this->adjacency_matrix[city_index].size(); weight_index++){
+            weight = this->adjacency_matrix[city_index][weight_index];
+            if (weight != NO_CONNECTION){
+                total_weight += this->adjacency_matrix[city_index][weight_index];
+            }
         }
     }
     return total_weight;
 }
 
 int Graph::breadth_first_path_weight(const std::string& begin, const std::string& end){
-    return 0;
+    begin, end;
+    return NO_CONNECTION;
 }
 
-// struct compare_function{
-//     bool operator()(HuffmanNode* node1, HuffmanNode* node2){
-//         return (node1->get_frequency() > node2->get_frequency());
-//     }
-// };
-
-// create a local struct to represent an edge: start, end, weight
-// use the indicies in the vertex_to_label in the disjoint set
-// 
-
-
 Graph Graph::kruskal(){
+    std::priority_queue<edge*, std::vector<edge*>, compare_function> queue;
+    std::vector<int> adj_vector;
+    size_t num_verticies = this->size();
+    DisjointSet connections(num_verticies);
 
-    
-    Graph mst(*this);
-    
+    for (size_t index = 0; index < num_verticies; index++){
+        connections.Make_Set(index); // creates the forest of trees
+        adj_vector = this->adjacency_matrix[index];
+        for (size_t connection_index = index + 1; connection_index < adj_vector.size(); connection_index++){ // make sure to skip dupes
+            edge* route = new edge(index, connection_index, adj_vector[connection_index]); 
+            queue.push(route); // adds all edges into the priority queue
+        }
+    }
+
+    Graph mst(*this); // uses copy constructor
+    while (!queue.empty()){ // essentially a c&p from kruskal algorithm
+        edge* route = queue.top(); queue.pop();
+        size_t city_1 = route->vertex_1,
+               city_2 = route->vertex_2;
+        if (connections.Find(city_1) != connections.Find(city_2)){
+            connections.Union(city_1, city_2);
+        }
+        else{
+            mst.adjacency_matrix[city_1][city_2] = NO_CONNECTION; // updates the two places in the adjacency matrix that the edge represents
+            mst.adjacency_matrix[city_2][city_1] = NO_CONNECTION;
+        }
+    }
     return mst;
 }
 
 std::ostream& operator<<(std::ostream& os, const Graph& g){
+    int weight;
+    for (size_t index_1 = 0; index_1 < g.adjacency_matrix.size(); index_1++){
+        for (size_t index_2 = index_1 + 1; index_2 < g.adjacency_matrix[index_1].size(); index_2++){
+            weight = g.adjacency_matrix[index_1][index_2];
+            if (weight != g.NO_CONNECTION){
+                os << g.vertex_to_label_map[index_1] << " to " << g.vertex_to_label_map[index_2] << " : " << g.adjacency_matrix[index_1][index_2] << "\n";
+            }
+        }
+    }
     return os;
 }
-
-struct edge{
-    std::string from;
-    std::string to;
-    int weight;
-    edge(std::string from, std::string to, int weight):
-    from(from), to(to), weight(weight){}
-};
