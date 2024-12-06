01dc7f1a76809470423cdf6e4d833fb97f7fb481
ANDREWIDEAPAD\03dre <pattona@southern.edu>
2024-12-06 00:02:26 -0500

    Implemented BFS path weight method + small ostream tweak

--- graph.cpp
+++ graph.cpp
@@ -70,8 +70,42 @@ int Graph::weight() const {
 }
 
 int Graph::breadth_first_path_weight(const std::string& begin, const std::string& end){
-    begin, end;
-    return NO_CONNECTION;
+    size_t start = this->label_to_vertex_map[begin];
+    size_t dest = this->label_to_vertex_map[end];
+
+    size_t num_cities = this->size();
+    std::vector<size_t> path;
+    size_t city;
+    int weight = 0;
+    std::vector<bool> visited(num_cities, false); // initializes an array with size 'num_cities' to all zeros
+
+    visited[start] = 1;
+    bool done = false;
+    std::queue<size_t> queue;
+    queue.push(start);
+
+    while(!queue.empty() && !done){
+        city = queue.back(); queue.pop();
+        path.push_back(city);
+        for (size_t _city = 0; _city < this->adjacency_matrix[city].size(); _city++){
+            if (this->adjacency_matrix[city][_city] != NO_CONNECTION && !visited[_city]){
+                visited[_city] = true;
+                queue.push(_city);
+                if (_city == dest){
+                    done = true;
+                    path.push_back(_city);
+                }
+            }
+        }
+    }
+
+    if (done){ 
+        for (size_t index = 0; index < path.size() - 1; index++){
+        weight += adjacency_matrix[path[index]][path[index + 1]];
+        }
+    } else { weight = NO_CONNECTION; } // if all paths were checked and dest was not found
+    
+    return weight;
 }
 
 Graph Graph::kruskal(){
@@ -118,25 +152,25 @@ std::ostream& operator<<(std::ostream& os, const Graph& g){
             city = g.vertex_to_label_map[index_1];
             os << std::right << std::setw(SPACE) << city + " ";
         }
-        os << std::right << std::setw(SPACE) << g.vertex_to_label_map[num_cities - 1] + " "<< '\n'; // puts last city
+        os << std::setw(SPACE) << g.vertex_to_label_map[num_cities - 1] + " "<< '\n'; // puts last city
 
-        os << std::setw(SPACE - 1) << ""; // spacer for header (will be resued for printing cities)
+        os << std::setw(SPACE - 1) << ""; // spacer for header
         os << std::left << std::setfill('-') << std::setw(SPACE * num_cities) << '+' << '\n'; // fills in the dashes
 
         for (size_t index_1 = 0; index_1 < num_cities; index_1++){
             city = g.vertex_to_label_map[index_1];
-            os << std::right << std::setfill(' ') << std::setw(SPACE) << city + " |";
+            os << std::right << std::setfill(' ') << std::setw(SPACE) << city + " |"; // left city label
             weights = g.adjacency_matrix[index_1];
             if (weights.size() > 0){
                 weight = weights[0];
-                if (weight == g.NO_CONNECTION){ str_weight = "-"; }
+                if (weight == g.NO_CONNECTION){ str_weight = "-"; } 
                 else { str_weight = std::to_string(weight); }
-                os << std::right << std::setw(SPACE - 1) << str_weight + " ";
+                os << std::setw(SPACE - 1) << str_weight + " ";
                 for (size_t weight_index = 1; weight_index < weights.size(); weight_index++){ // prints weights for the city
                     weight = weights[weight_index];
                     if (weight == g.NO_CONNECTION){ str_weight = "-"; }
                     else { str_weight = std::to_string(weight); }
-                    os << std::right << std::setw(SPACE) << str_weight + " ";
+                    os << std::setw(SPACE) << str_weight + " ";
                 }
             }
             os << "\n";
